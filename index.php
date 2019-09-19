0770f9f1ecf12f6cb137633dd6a51b2c1103045b
DanielHB <40381839+Daniel-Simiyu@users.noreply.github.com>
2019-09-19 15:26:06 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title>MY PHP PRACTICE</title>
+</head>
+<body>
+	<form action="INDEX1.php", method="post", enctype="multipart/form-data>
+
+	<div id="header">
+		<label>Food Item: </label>
+		<input type="text" name="fooditem">
+		<label>Upload Picture: </label>
+		<input type="file" name="image">
+		<label>Price: </label>
+		<input type="text" name="price">
+		<input type="submit" name="submit">
+	</div>
+	</form>
+</body>
